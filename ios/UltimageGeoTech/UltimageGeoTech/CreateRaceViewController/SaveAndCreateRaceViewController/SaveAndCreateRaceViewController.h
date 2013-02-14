dd8051a28f4d7adfb900a52722e7656782549542
Chirag Purohit <chirag.purohit@sunshineinfotech.com>
2013-02-14 08:10:05 +0530

    server_1

--- ios/UltimageGeoTech/UltimageGeoTech/CreateRaceViewController/SaveAndCreateRaceViewController/SaveAndCreateRaceViewController.h
+++ ios/UltimageGeoTech/UltimageGeoTech/CreateRaceViewController/SaveAndCreateRaceViewController/SaveAndCreateRaceViewController.h
@@ -7,13 +7,40 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import "AppDelegate.h"
 @interface SaveAndCreateRaceViewController : UIViewController<UITextFieldDelegate,UITextViewDelegate>
 {
-    IBOutlet UITextField* race_name_text_field;
-    IBOutlet UITextField* show_number_of_question_text_field;
+    IBOutlet UITextField* race_name_text_field;//10001
+    IBOutlet UITextField* show_number_of_question_text_field;//10002
     
-    IBOutlet UITextView*race_detail_textView;
+    IBOutlet UITextView*race_detail_textView;//10003
+    
+    NSMutableDictionary*race_extra_detail_decitionary;
+    
+    AppDelegate*app_delegate;
+    
+    int current_textfield_textview_tag;
+    
+    
+    //variabales for Webservices
+    
+    
+    NSArray *requestObjects;
+	NSArray *requestkeys;
+	NSDictionary *requestJSONDict;
+	NSMutableDictionary *finalJSONDictionary;
+	NSString *jsonRequest;
+	NSString *requestString;
+	NSData *requestData;
+	NSString *urlString;
+	NSMutableURLRequest *request;
+	NSData *returnData;
+	NSError *error;
+	SBJSON *json;
+	NSDictionary *responseDataDictionary;
+    //NSMutableArray *responseDataArray;//May be needed
+    
+    IBOutlet UIActivityIndicatorView*process_activity_indicator;
     
 }
 
--- ios/UltimageGeoTech/UltimageGeoTech/CreateRaceViewController/SaveAndCreateRaceViewController/SaveAndCreateRaceViewController.m
+++ ios/UltimageGeoTech/UltimageGeoTech/CreateRaceViewController/SaveAndCreateRaceViewController/SaveAndCreateRaceViewController.m
@@ -27,8 +27,8 @@
 {
     [super viewDidLoad];
     self.navigationItem.hidesBackButton = YES;
-    
-
+    race_extra_detail_decitionary = [[NSMutableDictionary alloc] init];
+    app_delegate = (AppDelegate*)[UIApplication sharedApplication].delegate;
     // Do any additional setup after loading the view from its nib.
 }
 
@@ -43,6 +43,43 @@
 -(IBAction)create_race_button_clicked:(id)sender
 {
     
+    switch (current_textfield_textview_tag)
+    {
+        case 1001:
+        {
+            
+            
+            [race_extra_detail_decitionary setObject:race_name_text_field.text forKey:@"race_name"];
+            break;
+        }
+        case 1002:
+        {
+            [race_extra_detail_decitionary setObject:race_detail_textView.text forKey:@"race_detail"];
+            break;
+        }
+        case 1003:
+        {
+            [race_extra_detail_decitionary setObject:show_number_of_question_text_field.text forKey:@"number_of_question"];
+            break;
+        }
+        default:
+        {
+            break;
+        }
+            
+    }
+    
+    
+    
+    
+    
+    [race_extra_detail_decitionary setObject:[NSNumber numberWithFloat:app_delegate.current_latitude] forKey:@""];
+    [race_extra_detail_decitionary setObject:[NSNumber numberWithFloat:app_delegate.current_longitued] forKey:@""];
+    
+    
+    
+    NSLog(@"\n race_extra_detail_decitionary = %@",race_extra_detail_decitionary);
+    NSLog(@"\n app_delegate = %@",app_delegate.current_race_question_array);
 }
 
 #pragma mark - share_race_button_clicked
@@ -56,11 +93,12 @@
 
 - (BOOL)textFieldShouldBeginEditing:(UITextField *)textField
 {
+    
     return TRUE;
 }
 - (void)textFieldDidBeginEditing:(UITextField *)textField
 {
-    
+    current_textfield_textview_tag = textField.tag;
 }
 - (BOOL)textFieldShouldEndEditing:(UITextField *)textField
 {
@@ -68,7 +106,24 @@
 }
 - (void)textFieldDidEndEditing:(UITextField *)textField
 {
+    switch (current_textfield_textview_tag)
+    {
+        case 1001:
+        {
+            [race_extra_detail_decitionary setObject:textField.text forKey:@"race_name"];
+             break;
+        }
+            case 1003:
+        {
+            [race_extra_detail_decitionary setObject:textField.text forKey:@"number_of_question"];
+             break;
+        }
+        default:
+        {
+            break;
+        }
     
+    }
 }
 
 
@@ -92,13 +147,19 @@
 
 - (void)textViewDidBeginEditing:(UITextView *)textView
 {
-    
+    current_textfield_textview_tag = textView.tag;
 }
 - (void)textViewDidEndEditing:(UITextView *)textView
 {
-    
+     [race_extra_detail_decitionary setObject:textView.text forKey:@"race_detail"];
 }
 
 
+#pragma mark - dealloc
+-(void)dealloc
+{
+    [race_extra_detail_decitionary release];
+    [super dealloc];
+}
 
 @end
